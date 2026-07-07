ae785dfc00d4a0c204b26f85c001a1a440ab2dcc
Chloractus <Chloractus@proton.me>
2026-07-07 18:19:24 -0400

    Heavily commented SSDP.py

--- NetCheck/util/SSDP.py
+++ NetCheck/util/SSDP.py
@@ -1,115 +1,77 @@
-"""
-========================================================
-                    SSDP Explained:
-========================================================
-
-Step 1: Send out an M-SEARCH packet (UDP + Plain text) to the
-        multicast group 239.255.255.250:1900. All UPnP devices
-        that are listening to the multicast group on the LAN
-        recieves it and replies with some HTTP headers including
-        a LOCATION URL and a SERVER string.
-
-Step 2: Perform an HTTP GET request with the LOCATION URL for each device.
-        The device then gives an XML file with the information we want. These
-        fields are human-readable (ex. <friendlyName> and <manufacturer>).
-
-Why/What is mutlicast?
-        Instead of trying to individually query all 254 IPs, we send one packet
-        to the "Multicast Group" that all UPnP devices join on startup. This
-        eliminates the need to send a bunch of packets as all of the UPnP devices
-        will reply if the Multicast Group is queried.
-What is an MX field???
-        Since we don't want to kill the network with the replies we get back, we
-        add an "MX Field" which makes each device pick a random delay between 0 and
-        "MX" seconds which spreads out the replies.
-"""
-
 import socket                                 # allows for socket operations to create the HTTP Socket for SSDP
 import xml.etree.ElementTree as ET            # XML support for python so we can parse XML descriptions
 from urllib import request                    # Allows for HTTP Requests
 from urllib.error import HTTPError, URLError  # Allows us to use HTTP and URL Error catches
 
 def fetch(location: str | None, server: str | None) -> dict:
-    # "fetch" has parameters "location" and "server"
-    # location has to be either a String or a None type
-    # server has to be either a String or a None type
-    # "fetch" has to return a dict type
-
-    #It's HTTP GET request time baby!
-
     """
-    =========================================================
-                    What dat fetch do?
-    =========================================================
+    Uses a Location URL to try and make an HTTP GET request for the information regarding the device.
+    If it doesn't find it, we default back to the name of the server.
 
-        This fetch function takes in the LOCATION URL and 
-        SERVER String, then it attempts to make an HTTP 
-        GET request to the LOCATION URL. It then parses the
-        XML to get the values we want. If the LOCATION URL
-        doesn't exist or doesn't return what we want,
-        we fall back to using the SERVER String.
+    Args:
+        location: A String representing the URL for a UPnP Device's information
+        server: A String representing the server name for a given UPnP Device
+
+    Returns:
+        Dictionary. This function returns a Dictionary of information relating to a given UPnP Device.
     """
-    wanted = {
-        'friendlyName' : None,
-        'manufacturer' : None,
-        'modelName' : None,
-        'modelNumber' : None
+
+    wanted = {                        #A Dictionary for the information we want to pull from the location URL
+        'friendlyName' : None,        #'Freindly Name' is the name assigned to the device (similar to DNS)
+        'manufacturer' : None,        #'Manufacturer' is usually the name of the company that produced the device
+        'modelName' : None,           #'Model Name' is usually the name of the generic model of the device
+        'modelNumber' : None          #'Model Number' is usually a number that belongs to the specific model the device is
     }
 
-    if location:
+    if location:          #First, checks to make sure that we actually have a location
         try:
-            #Performs HTTP GET at "loaction" and waits "timeout" seconds for a reply
-            with request.urlopen(location, timeout=2) as resp:
-                xmlData = resp.read()
+            with request.urlopen(location, timeout=2) as resp:      #Performs HTTP GET at "loaction" and waits "timeout" seconds for a reply
+                xmlData = resp.read()                               #If it gets a reply, it stores the XML data in a variable
 
-            #Parses the XML data into a tree of Elements
-            #Throws a fit if it isn't a valid XML (ET.ParseError)
-            root = ET.fromstring(xmlData)
+            root = ET.fromstring(xmlData)   #Parses the XML data into a tree of Elements
 
-            #ET.iter() does a depth-first search through all the elements in the tree
-            for elem in root.iter():
+            for elem in root.iter():        #ET.iter() does a depth-first search through all the elements in the tree
 
-                #The elements return a format like this: "{urn:schemas-upnp-org:device-1-0}friendlyName"
-                #But since we only want "friendlyName" if it exists,
-                #We split at '}' and only keep the last part [-1].
-                #Then, if there isn't a '}' we just grab the whole statement.
-                tag = elem.tag.split('}', 1)[-1] if '}' in elem.tag else elem.tag
+                tag = elem.tag.split('}', 1)[-1] if '}' in elem.tag else elem.tag    #Removes the formatting stuff from the XML and takes just the data from each tag
 
-                #Only record the Element if it is one of the elements we want,
-                #The element isn't empty, and we haven't recorded that element yet
-                if tag in wanted and elem.text and wanted[tag] is None:
-                    wanted[tag] = elem.text.strip()
+                if tag in wanted and elem.text and wanted[tag] is None:      #Checks to make sure that the element we are on is one we actually want
+                    wanted[tag] = elem.text.strip()                          #Strips it down to just the stuff we want and records it
                 
-                #Early exit if all our fields are found.
-                if all(v is not None for v in wanted.values()):
-                    break
+                
+                if all(v is not None for v in wanted.values()):   #Checks to see if we already have all the elements we want
+                    break                                         #If we do, then we don't need to keep looking and we can move on
                     
-        # URLError happens when we can't reach the device
-        # HPPTError happens when the device returns an HTTP status code that isn't a 2xx
-        except (URLError, HTTPError):
+        except (URLError, HTTPError):       #URLError happens when we can't reach the device and HTTPError happens when the device returns an HTTP status code that isn't a 2xx
             pass
         
-        # ET.ParseError happens when the XML file is invalid
-        except ET.ParseError:
+        except ET.ParseError:               #ET.ParseError happens when the XML file is invalid
             pass
         
-        # OSError is for low-level socket errors
-        # ValueError happens when we get a url that isn't a String or a None type
-        except (OSError, ValueError):
+        
+        except (OSError, ValueError):       #OSError is for low-level socket errors, ValueError happens when we get a url that isn't a String or a None type
             pass
     
-    return {
+    return {                                                        #Returns a Dictionary of all the stuff we found for the given UPnP Device
         'name' : wanted['friendlyName'] or server or 'N/A',
         'manufacturer' : wanted['manufacturer'] or 'N/A',
         'model' : wanted['modelName'] or 'N/A',
         'server' : server or 'N/A'
     }
 
-def SSDP(ip: str) -> dict[str, dict]:
+def SSDP(ip: str) -> dict:
+    """
+    Handles sending out a M-SEARCH packet to find out if a given IPv4 Host is a UPnP Device.
+    It also handles extracting the URL from the reply.
 
-    #Craft and send SSDP M-SEARCH packet to multicast group + Capture all replies
+    Args:
+        ip: A String representing the IPv4 Address that we are sending the packet to
 
-    msearch = (
+    Returns:
+        Dictionary. This function returns a Dictionary that contains the relevant information for
+        the given IPv4 Address.
+    """
+
+    msearch = (                      #Craft the SSDP M-SEARCH packet to the given IPv4 Address
         "M-SEARCH * HTTP/1.1\r\n"
         f"HOST: {ip}:1900\r\n"
         'MAN: "ssdp:discover"\r\n'
@@ -118,64 +80,50 @@ def SSDP(ip: str) -> dict[str, dict]:
         "\r\n"
     ).encode('utf-8')
 
-    # -- Creates Socket --
-    # AF_INET means it uses the IPv4 addresses
-    # SOCK_DGRAM specifies that we are using UDP
-    # IPPROTO_UDP is the bigger, more agressive version of SOCK_DGRAM
-    # SOCK_DGRAM suggest UDP while IPPROTO_UDP forces it
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)   #Creates a socket object that is IPv4 and UDP
 
-    # SO_REUSEADDR allows us to bind to port 1900 even if a previous instance is still using it
-    # Helps to make sure that we can still use the script even if we used it recently
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                    #Makes sure that we can do still use the correct port even if it was used recently
 
-    # IP_MULTICAST_TTL determines how many routers the multicast can hop.
-    # 2 is a save bet for a LAN but can be changed to fit larger networks
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)                #Safety net to make sure that the packet doesn't travel outside of the LAN
 
-    """ POTENTIALLY ADD ARG TO CHANGE IP_MULTICAST_TTL"""
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+    sock.settimeout(3)                  # Wait "timeout" seconds before moving on, with or without results
 
-    # Wait "timeout" seconds before moving on, with or without results
-    sock.settimeout(3)
+    sock.sendto(msearch, (ip, 1900))    # Send out the packet
 
-    #Send it to the multicast
-    sock.sendto(msearch, (ip, 1900))
-
-    raw = {}
+    raw = {}      #A Dictionary to hold the results from the packet
 
     try:
-        while True:
+        while True:         #Loop through this until we timeout
             try:
-                data, (src_ip, ips) = sock.recvfrom(65535)
+                data, (src_ip, ips) = sock.recvfrom(65535)     #Recieve the reply from the packet we sent
 
-                #Continue if we already did this IP
-                if src_ip in raw:
+                if src_ip in raw:         #Skip the IPv4 Address if it is already done (basically just a catch to make sure we don't repeat information)
                     continue
 
-                location = None
-                server = None
+                location = None           #Set location to None by default
+                server = None             #Set server to None by default
 
-                for line in data.decode('utf-8', errors='replace').split('\r\n'):
-                    upper = line.upper()
+                for line in data.decode('utf-8', errors='replace').split('\r\n'):     #For each line in the reply packet
+                    upper = line.upper()                                              #Make the line all uppercase
 
-                    if upper.startswith('LOCATION:'):
-                        location = line[9:].strip()
+                    if upper.startswith('LOCATION:'):             #Look for the location information
+                        location = line[9:].strip()               #If we find it, we take it
 
-                    elif upper.startswith('SERVER:'):
-                        server = line[7:].strip()
+                    elif upper.startswith('SERVER:'):             #Look for the server information
+                        server = line[7:].strip()                 #If we find it, we take it
 
-                raw[src_ip] = (location, server)
+                raw[src_ip] = (location, server)                  #Add the IPv4 Address and the information to 'raw' so that we dont repeat it
 
-            except socket.timeout:
-                break
+            except socket.timeout:            #If we hit the timeout
+                break                         #We break
 
-    except OSError:
-        pass
+    except OSError:          #If we run into any OSError
+        pass                 #We move on past it
 
     finally:
-        sock.close()
+        sock.close()         #Once we are all done, we close the socket
 
-    if not raw:
-        return fetch(None, None)
-    else:
-        return fetch(location, server)
+    if not raw:                         #If we got nothing in raw
+        return fetch(None, None)        #We return fetch with None as both the location and server
+    else:                               #If we DO get something in raw
+        return fetch(location, server)  #We return fetch with the location and server we found
